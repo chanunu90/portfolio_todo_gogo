d8e49459d7f0aefb614106bf162fd3cf4a1ba353
chanunu90 <128343652+chanunu90@users.noreply.github.com>
2023-08-25 12:14:49 +0900

    0825

---
+++ src/main/java/org/astro/portfolio/controller/ReplyController.java
@@ -0,0 +1,28 @@
+package org.astro.portfolio.controller;
+
+import java.util.Map;
+
+import org.astro.portfolio.dto.PageRequestDTO;
+import org.astro.portfolio.dto.PageResponseDTO;
+// import org.astro.portfolio.dto.ReplyDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/reply")
+public class ReplyController {
+
+    // @GetMapping("/list")
+    // public PageResponseDTO<ReplyDTO> getReplyList(PageRequestDTO pageRequestDTO){
+        
+    //     return null;
+
+    // }
+    
+}
---
+++ src/test/java/org/astro/portfolio/ReplyMapperTests/ReplyMapperTests.java
@@ -0,0 +1,23 @@
+package org.astro.portfolio.ReplyMapperTests;
+
+import org.astro.portfolio.mappers.ReplyMappers;
+// import org.astro.portfolio.mappers.ReplyMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class ReplyMapperTests {
+
+    @Autowired
+    private ReplyMappers replyMapper;
+
+    @Test
+    public void testTimasde(){
+        log.info(replyMapper.time());
+    }
+    
+}
